1b73c438ce61afb7d7f14b7598b2fac4e322cbfe
LordEagle <r.wermelinger@gmail.com>
2022-01-12 18:36:25 +0000

    generalize docker+ it's compose service setup

---
+++ docker/install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+dockerDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+instDocker(){
+  if ! [ -x "$(command -v docker )" ]; then
+    echo "INSTALLING docker"
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    sudo usermod -aG docker $USER
+  fi
+}
+
+instCompose(){
+  if ! [ -x "$(command -v docker-compose )" ]; then
+    echo "INSTALLING docker-compose"
+    sudo apt install -y libffi-dev libssl-dev
+    sudo apt install -y python3-dev
+    sudo apt install -y python3 python3-pip
+    sudo pip3 install docker-compose
+  fi
+}
+
+instComposeService(){
+  service="$1" #technical name
+  serviceName="$2" # human readable
+  qualified="/etc/systemd/system/${service}.service"
+  if ! [ -f "${qualified}" ]; then
+    echo "INSTALLING ${service} service"
+    srvFile="${dockerDir}/${service}.service"
+    cat "${dockerDir}/compose.service.template" | 
+      sed -e "s|__WORKDIR__|${PWD}|g" |
+      sed -e "s|__SERVICE__|${serviceName}|g" |
+      tee "${srvFile}"
+    sudo mv "${dockerDir}/${service}.service" "${qualified}"
+    sudo systemctl enable ${service}
+    sudo systemctl start ${service}
+    sleep 3 # mini time to start img resolving
+  fi
+}
+
