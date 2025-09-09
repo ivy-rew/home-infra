74445f5f03bba506951d6285130fdaa9d5b00e56
LordEagle <r.wermelinger@gmail.com>
2025-09-09 20:53:34 +0200

    drop 'nextcloudpi' in favour of plain 'arm' compose

--- nextcloudarm/install.sh
+++ nextcloudarm/install.sh
@@ -9,12 +9,11 @@ instNextCloudService(){
 }
 
 nextConfigAlias(){
-  hasAlias=`alias | grep ncp-config`
+  hasAlias=`alias | grep nca`
   if [ -z "${hasAlias}" ]; then
     aliases="$HOME/.bash_aliases"
-    echo 'alias ncp="docker exec -it nextcloudpi"' >> "${aliases}"
-    echo 'alias ncp-config="ncp ncp-config"' >> "${aliases}"
-    echo 'alias occ="ncp sudo -u www-data php /data/nextcloud/occ"' >> "${aliases}"
+    echo 'alias nca="env --chdir ~/home-infra/nextcloudarm -S docker compose"' >> "${aliases}"
+    echo 'alias occ="nca exec --user www-data app php occ"' >> "${aliases}"
     . "${aliases}" # re-load
   fi
   echo "ALIAS ncp... ready"
